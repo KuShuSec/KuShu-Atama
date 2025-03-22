4768f4e0f9f65afa29a00bc45a89da762f2caf61
Graham Gold <graham@cirriustech.co.uk>
2025-03-22 19:43:19 +0000

    v0.0.1

---
+++ src/Entra_Hybrid_Attack_And_Defence.mm
@@ -0,0 +1,351 @@
+﻿<map version="0.9.0">
+  <node TEXT="Entra Hybrid Attack&#10;And Defence">
+    <node TEXT="Initial Access">
+      <node TEXT="AiTM Phishing">
+        <node TEXT="Offensive Tools">
+          <node TEXT="Evilginx2"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Device Code Flow Abuse">
+        <node TEXT="Offensive Tools">
+          <node TEXT="AADInternals"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Illicit OAuth&#10;Consent Grants">
+        <node TEXT="Offensive Tools">
+          <node TEXT="ROADtools"></node>
+          <node TEXT="PowerZure"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Device Join Abuse&#10;(Hybrid &amp; Azure AD Join)">
+        <node TEXT="Offensive Tools">
+          <node TEXT="AADInternals"></node>
+          <node TEXT="ROADtools"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Device Compliance &amp;&#10;Conditional Access Abuse">
+        <node TEXT="Offensive Tools">
+          <node TEXT="AADInternals"></node>
+          <node TEXT="TokenTactics"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Password Spraying">
+        <node TEXT="Offensive Tools">
+          <node TEXT="AADInternals"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Azure AD Connect Abuse">
+        <node TEXT="Offensive Tools">
+          <node TEXT="AADInternals"></node>
+          <node TEXT="ROADtools"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Redirect URL Hijack">
+        <node TEXT="Offensive Tools"></node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Bring Your Own&#10;Vulnerable App">
+        <node TEXT="Offensive Tools"></node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Dynamic Consent Abuse">
+        <node TEXT="Offensive Tools"></node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+    </node>
+    <node TEXT="Credential Theft">
+      <node TEXT="Pass-the-Hash">
+        <node TEXT="Offensive Tools">
+          <node TEXT="Mimikatz"></node>
+          <node TEXT="Impacket"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Session Cookie Theft">
+        <node TEXT="Offensive Tools">
+          <node TEXT="Evilginx2"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Pass-the-PRT">
+        <node TEXT="Offensive Tools">
+          <node TEXT="ROADtools"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Golden SAML">
+        <node TEXT="Offensive Tools">
+          <node TEXT="ADFSDump &amp; ADFSpoof"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="AD CS Misconfiguration&#10;(Golden Certificate)">
+        <node TEXT="Offensive Tools">
+          <node TEXT="Certify"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+    </node>
+    <node TEXT="Privilege Escalation and&#10;Lateral Movement">
+      <node TEXT="Golden Ticket">
+        <node TEXT="Offensive Tools">
+          <node TEXT="Mimikatz"></node>
+          <node TEXT="Rubeus"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Silver Ticket">
+        <node TEXT="Offensive Tools">
+          <node TEXT="Rubeus"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Malicious Certificate Based Authentication">
+        <node TEXT="Offensive Tools">
+          <node TEXT="AADInternals"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="OAuth Token Abuse">
+        <node TEXT="Offensive Tools">
+          <node TEXT="AADInternals"></node>
+          <node TEXT="ROADtools"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="1st Party App Consent Abuse">
+        <node TEXT="Offensive Tools">
+          <node TEXT="Family Of Client IDs (FOCI)">
+            <node TEXT="AADInternals"></node>
+            <node TEXT="ROADtools"></node>
+          </node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Application&#10;Administrator&#10;Abuse">
+        <node TEXT="Offensive Tools"></node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Azure RBAC Abuse">
+        <node TEXT="Offensive Tools"></node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="T0 Equivalent API Permissions Abuse">
+        <node TEXT="Offensive Tools"></node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)">
+          <node TEXT="Restrict and Audit Use of Scopes">
+            <node TEXT="Directory.ReadWrite.All"></node>
+            <node TEXT="RoleManagement.ReadWrite.Directory"></node>
+            <node TEXT="AppRoleAssignment.ReadWrite.All"></node>
+            <node TEXT="UserAuthenticationMethod.ReadWrite.All (FIDO2/TAP Only)"></node>
+          </node>
+        </node> 
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="OverPermissioned Application Scopes">
+        <node TEXT="Offensive Tools"></node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+    </node>
+    <node TEXT="Persistence">
+      <node TEXT="Adding Rogue&#10;Federation Trusts">
+        <node TEXT="Offensive Tools">
+          <node TEXT="AADInternals"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="OAuth App Consent Abuse">
+        <node TEXT="Offensive Tools">
+          <node TEXT="MicroBurst"></node>
+          <node TEXT="PowerZure"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="MFA Fatigue">
+        <node TEXT="Offensive Tools">
+          <node TEXT="Evilginx2"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Hidden Client Secrets&#10;(App Reg - API only CRUD)">
+        <node TEXT="Offensive Tools"></node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="User Managed Identity Federation">
+        <node TEXT="Offensive Tools"></node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="offline_access scope (Refresh Token Abuse)">
+        <node TEXT="Offensive Tools"></node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Temporary Access Pass">
+        <node TEXT="Offensive Tools"></node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+    </node>
+    <node TEXT="Evasion">
+      <node TEXT="MFA Fatigue">
+        <node TEXT="Offensive Tools">
+          <node TEXT="Evilginx2"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="ROPC (Legacy Auth Exploitation - No MFA)">
+        <node TEXT="Offensive Tools">
+          <node TEXT="AADInternals"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Fake MFA">
+        <node TEXT="Offensive Tools">
+          <node TEXT="External&#10;Authentication&#10;Methods">
+            <node TEXT="ROADoidc"></node>
+          </node> 
+          <node TEXT="Silver Ticket"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Fake Compliant Device">
+        <node TEXT="Offensive Tools"></node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Fake Device Join">
+        <node TEXT="Offensive Tools"></node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Incomplete Logging">
+        <node TEXT="Offensive Tools"></node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+    </node>
+    <node TEXT="Multi-stage Attack Chaining">
+      <node TEXT="Attack Path Mapping">
+        <node TEXT="Offensive Tools">
+          <node TEXT="BloodHound"></node>
+          <node TEXT="AzureHound"></node>
+          <node TEXT="ROADtools"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Orchestration Frameworks">
+        <node TEXT="Offensive Tools">
+          <node TEXT="Cobalt Strike"></node>
+          <node TEXT="Mythic Framework"></node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+    </node>
+    <node TEXT="Reconnaissance">
+      <node TEXT="Unauthenticated">
+        <node TEXT="Offensive Tools">
+          <node TEXT="OIDC.well-known">
+            <node TEXT="Curl/Postman/BurpSuite"></node>
+          </node> 
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+      <node TEXT="Authenticated">
+        <node TEXT="Offensive Tools">
+          <node TEXT="Azure AD graph">
+            <node TEXT="AADInternals"></node>
+            <node TEXT="ROADrecon"></node>
+          </node> 
+          <node TEXT="Microsoft Graph">
+            <node TEXT="AADInternals"></node>
+            <node TEXT="ROADrecon"></node>
+          </node>
+        </node>
+        <node TEXT="Preventative&#10;Controls&#10;(Hardening)"></node>
+        <node TEXT="Preventative Tools"></node>
+        <node TEXT="Detection Sources"></node>
+      </node>
+    </node>
+  </node>
+</map>
